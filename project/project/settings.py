2541b654131ecdaa3630338642cadbc893c743c4
meetprajap <meetp5986@gmail.com>
2025-06-18 22:50:57 +0530

    done

--- project/project/settings.py
+++ project/project/settings.py
@@ -138,3 +138,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://djangop-2.onrender.com']
